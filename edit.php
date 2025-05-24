4c0b18d43be7645a014e8a6972ec64d5c1cd4597
cleosaav <vania06putrii@gmail.com>
2025-05-24 22:23:45 +0700

    .

--- edit.php
+++ edit.php
@@ -161,16 +161,5 @@ $alamat = $_POST['alamat'];
         <button type="submit" class="daftar" name="edit">Ubah Data Diri</button>
       </form>
     </div>
-    <!-- ?php
-      else {
-      $sql = "INSERT INTO users (id, email, password, nama_lengkap, no_telp, provinsi, kabupaten, kecamatan, desa, alamat)
-              VALUES ('','$email', '$password', '$nama', '$no_handphone', '$provinsi', '$kabupaten', '$kecamatan', '$desa' , '$alamat')";
-      if (mysqli_query($connect, $sql)) {
-        header('location:regist.php');
-      } else {
-        echo "<div class='alert alert-danger mt-3'>Gagal menyimpan data: " . mysqli_error($connect) . "</div>";
-      }
-    }
-    ?> -->
   </body>
 </html>
